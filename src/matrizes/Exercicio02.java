5e8cf6cfa9de08a2d8d168ce0cbf40d23827d007
Anna May <annamayatk@gmail.com>
2026-04-28 16:40:07 -0300

    mais exercícios matrizes

--- src/matrizes/Exercicio02.java
+++ src/matrizes/Exercicio02.java
@@ -1,10 +1,23 @@
 package matrizes;
 
+import java.util.Scanner;
+
 public class Exercicio02 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int[][] matriz = new int[n][n];
+		int[] soma = new int[n];
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				matriz[i][j] = sc.nextInt();
+				soma[i] += matriz[i][j];
+			}
+			System.out.println(soma[i]);
+		}
 
+		sc.close();
 	}
 
 }
